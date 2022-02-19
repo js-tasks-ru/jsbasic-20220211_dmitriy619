8b0e7fb26dd7ed3c7d500908556280f94205b1d2
Dimmaster87 <shpotya12@gmail.com>
2022-02-19 18:13:06 +0300

    3 модуль

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,10 @@
 function showSalary(users, age) {
   // ваш код...
+  let str=[];
+  for (user in users) {
+    if (users[user].age <= age) {
+      str.push(users[user].name +', ' + users[user].balance); 
+    } 
+  }
+  return str.join('\n');
 }
