e7d41561b1a73bdbb3e2d15b1e081de20de3583d
Dimmaster87 <shpotya12@gmail.com>
2022-02-20 14:35:25 +0300

    5 модуль

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,10 @@
 function hideSelf() {
   // ваш код...
+  let button = document.querySelector('.hide-self-button');
+  button.onclick = function(){
+      //your handler here
+      button.setAttribute('hidden','true')
+  }
+    
+  
 }
